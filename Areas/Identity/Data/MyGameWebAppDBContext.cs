396ab3ddc38c52835a0c426c9c77eb8b651e2d42
David Favier <ac100784@avcol.school.nz>
2021-09-09 23:59:45 +1200

    I have worked on creating the social media section and the footer section but will need to tweak the size of the images in the inline file. Still need to work on the preloader to cover the entire screen before seeing the home page.

---
+++ Areas/Identity/Data/MyGameWebAppDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyGameWebApp.Areas.Identity.Data;
+
+namespace MyGameWebApp.Data
+{
+    public class MyGameWebAppDBContext : IdentityDbContext<MyGameWebAppUser>
+    {
+        public MyGameWebAppDBContext(DbContextOptions<MyGameWebAppDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyGameWebApp.Areas.Identity.Data;
+using MyGameWebApp.Data;
+
+[assembly: HostingStartup(typeof(MyGameWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace MyGameWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MyGameWebAppDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MyGameWebAppDBContextConnection")));
+
+                services.AddDefaultIdentity<MyGameWebAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MyGameWebAppDBContext>();
+            });
+        }
+    }
+}
